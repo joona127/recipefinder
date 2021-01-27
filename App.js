a4248fe1ce150f921cc1f4723d7601d4f5632d64
joona127 <65039165+joona127@users.noreply.github.com>
2021-01-28 01:28:54 +0200

    lisäyksiä

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
+  const [ingredients, setIngredients] = React.useState('');
+  const [thumbnail, setThumbnail] = React.useState('');
+  const [recipes, setRecipes] = React.useState([]);
+
+  const getRecipes = () => {
+    const url = 'http://www.recipepuppy.com/about/api/?i='+ ingredients;
+    fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      setRecipes(responseJson);
+    })
+  
+  .catch((error) => {
+    Alert.alert('Error', error);
+  })
+}
+
+const listSeparator = () => {
+  return (
+    <View
+      style={{
+        height: 1,
+        width: "80%",
+        backgroundColor: "#CED0CE",
+        marginLeft: "10%"
+      }}
+    />
+  );
+};
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList>
+        keyExtractor={item => item.id}
+        renderItem{({item}) => 
+        
+  
+      </FlatList>
+  
     </View>
   );
 }
